8000014572881128afc130bc6f54907e17edfe68
SahanaSukumar <ssuk0011@student.monash.edu>
2025-06-02 22:40:30 +1000

    Configured Cognito

--- birdtag-ui/src/App.js
+++ birdtag-ui/src/App.js
@@ -3,10 +3,64 @@ import './App.css';
 import React from 'react';
 import UploadForm from './components/UploadingFiles';
 import SpeciesSearch from './components/SpeciesSearch.js';
+import { useAuth } from "react-oidc-context";
 
 function App() {
-  // return (
-  //   <div className="App">
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    const clientId = "4h5dp0m9cjkkr2ld7lc98fhufe";
+    const logoutUri = "http://localhost:3000";
+    const cognitoDomain = "https://ap-southeast-2frkp14bgu.auth.ap-southeast-2.amazoncognito.com";
+    auth.removeUser();
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  
+    // Remove local tokens
+    
+
+  // Redirect to the logout endpoint, and after logout, automatically redirect back to Hosted UI login
+    
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+  
+  if (!auth.isAuthenticated) {
+    return (
+      <div className="App">
+        <h1>BirdTag App</h1>
+        <p>You must sign in to continue</p>
+        <button onClick={() => auth.signinRedirect()}>Sign In</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <h1>BirdTag App</h1>
+      <p>Welcome, {auth.user?.profile.email}</p>
+      <button onClick={signOutRedirect}>Sign Out</button>
+      <UploadForm />
+      <hr />
+      <SpeciesSearch />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+//   <div className="App">
   //     <header className="App-header">
   //       <img src={logo} className="App-logo" alt="logo" />
   //       <p>
@@ -23,14 +77,3 @@ function App() {
   //     </header>
   //   </div>
   // );
-  return (
-    <div className="App">
-      <h1>BirdTag App</h1>
-      <UploadForm />
-      <hr/>
-      <SpeciesSearch />
-    </div>
-  );
-}
-
-export default App;
--- birdtag-ui/src/index.js
+++ birdtag-ui/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from "react-oidc-context";  
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_fRKP14BgU",
+  client_id: "4h5dp0m9cjkkr2ld7lc98fhufe",
+  redirect_uri: "http://localhost:3000",
+  response_type: "code",
+  scope: "phone openid email",
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
